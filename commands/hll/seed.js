d0e56a11bcffff45c2f04a46421860eca429a6a4
DarkMukke <hello@darkmukke.com>
2023-05-03 07:58:31 +0100

    - Adding option to make showing thresholds and map changes in the public channel optional. - Cleaned up re-used variables, for better readability. - Added Todo list for feature tracking

--- commands/hll/seed.js
+++ commands/hll/seed.js
@@ -62,6 +62,16 @@ module.exports = {
             option.setName('vip')
                 .setDescription('The x amount of first players that get a VIP slot. Default 0.')
         )
+
+        .addStringOption(option =>
+            option.setName('show-thresholds')
+                .setDescription('Set to 1 if you want to show when thresholds are reached in the public channel. Default 0.')
+        )
+
+        .addStringOption(option =>
+            option.setName('show-mapchanges')
+                .setDescription('Set to 0 if you don\'t want to show when the map changes in the public channel. Default 1.')
+        )
     ,
     async execute(interaction) {
 
@@ -77,6 +87,8 @@ module.exports = {
         }
 
         const vip = interaction.options.getString('vip') ?? 0;
+        const showThresholds = interaction.options.getString('show-thresholds') ?? 0;
+        const showMapChanges = interaction.options.getString('show-mapchanges') ?? 1;
         const thresholds = interaction.options.getString('threshold').split('|');
         const battlemetrics = interaction.options.getString('battlemetrics');
         const server = interaction.options.getString('server');
@@ -84,12 +96,14 @@ module.exports = {
 
         got(`${BM_API}${battlemetrics}`, { json: true }).then(async response => {
             const hlldata = response.body.data.attributes;
+            let maxPlayers = hlldata.maxPlayers;
+            let currPlayers = hlldata.players;
             let reply = `Seeding started by ${interaction.user.username}, for Server ${server}, BM id ${battlemetrics}.\n` +
                 blockQuote(
                     `Server: ${hlldata.name}\n` +
-                    `Players: ${hlldata.players}/${hlldata.maxPlayers}\n` +
-                    `Thresholds: ${thresholds}\n` +
-                    `Map: ${hlldata.details.map}`
+                    `Players: ${currPlayers}/${maxPlayers}\n` +
+                    `Thresholds: ${thresholds} | Public Thresholds: ${showThresholds ? 'Yes' : 'No'}\n` +
+                    `Map: ${hlldata.details.map} | Public Map Changes: ${showMapChanges ? 'Yes' : 'No'}`
                 )
             const vip_message = vip > 0 ? 'GRATIS VIP SLOTEN VOOR DE EERSTE {{vip}} SEEDERS\n\n' : ''
             await interaction.reply(reply);
@@ -114,16 +128,18 @@ module.exports = {
                 serverdeeds: hlldata.name
             });
             seed_channel.send(raw_message);
-            if(hlldata.players === hlldata.maxPlayersplayers) {
-                //we dont start the seeding loop if the the server is already full
-                admin_channel.send(`Seeding #${server} voltooid: Server zit vol (${hlldata.players}/${loop_hlldata.maxPlayersplayers}).`)
+            if(currPlayers === maxPlayers) {
+                //we don't start the seeding loop if the server is already full
+                admin_channel.send(`Seeding #${server} voltooid: Server zit vol (${currPlayers}/${maxPlayers}).`)
                 return;
             }
-            let current_threshold_index = findThresholdIndex(thresholds, hlldata.players);
+            let current_threshold_index = findThresholdIndex(thresholds,currPlayers);
             if(current_threshold_index === thresholds.length -1 ) {
                 //we dont start the seeding loop when the highest threshold is already reached
-                admin_channel.send(`Seeding #${server} voltooid: Max threshold (${hlldata.players}/${thresholds[current_threshold_index]}).`)
-                seed_channel.send(`Al ${hlldata.players} spelers online.`);
+                admin_channel.send(`Seeding #${server} voltooid: Max threshold (${currPlayers}/${thresholds[current_threshold_index]}).`)
+                if( showThresholds > 0 ) {
+                    seed_channel.send(`Al ${currPlayers} spelers online.`);
+                }
                 return;
             }
             let current_map = hlldata.details.map;
@@ -132,20 +148,28 @@ module.exports = {
                     let loop_hlldata = loop_response.body.data.attributes;
                     let new_map = loop_hlldata.details.map;
                     let new_players = loop_hlldata.players;
-                    if(current_map !== new_map) {
-                        seed_channel.send(`Nieuwe map op Server #${server}: ${new_map}.`);
+                    if(current_map !== new_map && ( new_map.includes('WARFARE') || new_map.includes('OFFENSIVE') )) {
+                        if( showMapChanges > 0 ) {
+                            seed_channel.send(`Nieuwe map op Server #${server}: ${new_map}.`);
+                        } else {
+                            admin_channel.send(`Seeding #${server} New Map: ${new_map}.`)
+                        }
                         current_map = new_map;
                     }
-                    let new_threshold_index = findThresholdIndex(thresholds, loop_hlldata.players);
+                    let new_threshold_index = findThresholdIndex(thresholds, new_players);
                     if(new_threshold_index > current_threshold_index ) {
-                        seed_channel.send(`Al ${loop_hlldata.players} spelers online.`);
-                    } else if(loop_hlldata.players === loop_hlldata.maxPlayersplayers ) {
+                        if( showThresholds > 0 ) {
+                            seed_channel.send(`Al ${new_players} spelers online.`);
+                        } else {
+                            admin_channel.send(`Seeding #${server} threshold: (${new_players}/${maxPlayers}).`)
+                        }
+                    } else if(new_players === maxPlayers ) {
                         // server full, lets cancel the seeder
-                        admin_channel.send(`Seeding #${server} voltooid: Server zit vol (${loop_hlldata.players}/${loop_hlldata.maxPlayersplayers}).`)
+                        admin_channel.send(`Seeding #${server} voltooid: Server zit vol (${new_players}/${maxPlayers}).`)
                         clearInterval(seeder);
                     } else if(new_threshold_index === thresholds.length -1 ) {
                         //we are out of thresholds, we reached the end of seeding.
-                        admin_channel.send(`Seeding #${server} voltooid: Max threshold (${loop_hlldata.players}/${thresholds[new_threshold_index]}).`)
+                        admin_channel.send(`Seeding #${server} voltooid: Max threshold (${new_players}/${thresholds[new_threshold_index]}).`)
                         clearInterval(seeder);
                     }
                     current_threshold_index = new_threshold_index
